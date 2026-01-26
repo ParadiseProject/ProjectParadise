2907c4d880d1a7d36ad8a6550294f57b89c4422e
YouSungMin <xormraoghk@gmail.com>
2026-01-26 17:52:34 +0900

    Feat : DT_UnitStructs 추가

---
+++ Source/Paradise/Public/Data/Structs/DT_UnitStructs.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "DT_UnitStructs.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCharacterStats : public FTableRowBase
+{
+	GENERATED_BODY()
+};
