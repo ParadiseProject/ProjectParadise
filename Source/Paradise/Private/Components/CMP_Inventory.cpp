c0366b71ddbba2ba6d6d98a254f11f8162056a90
KimSungHyun <144310761+SUNMUNG@users.noreply.github.com>
2026-01-29 13:14:16 +0900

    0129 - 인벤토리 컴포넌트 헤더 구조 설계

--- Source/Paradise/Private/Components/CMP_Inventory.cpp
+++ Source/Paradise/Private/Components/CMP_Inventory.cpp
@@ -8,11 +8,33 @@ UCMP_Inventory::UCMP_Inventory()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
 
+void UCMP_Inventory::InitInventory(const TArray<FOwnedHeroData>& InHeroes, const TArray<FOwnedFamiliarData>& InFamiliars, const TArray<FOwnedItemData>& InItems)
+{
+}
+
+void UCMP_Inventory::AddItem(FName ItemID, int32 Count)
+{
+}
+
+bool UCMP_Inventory::RemoveItem(FName ItemID, int32 Count)
+{
+	return false;
+}
+
+void UCMP_Inventory::AddHero(FName HeroID)
+{
+}
+
+
+int32 UCMP_Inventory::GetItemQuantity(FName ItemID) const
+{
+	return int32();
+}
 
 // Called when the game starts
 void UCMP_Inventory::BeginPlay()
@@ -24,11 +46,5 @@ void UCMP_Inventory::BeginPlay()
 }
 
 
-// Called every frame
-void UCMP_Inventory::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
 
