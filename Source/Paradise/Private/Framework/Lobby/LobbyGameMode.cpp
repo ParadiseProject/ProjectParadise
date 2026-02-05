c47257c2bd26484d666aa8ea47ea8f4b39691b4b
kdjh2002 <kdjh2002@naver.com>
2026-02-05 16:52:23 +0900

    feat : 로비 게임모드/컨트롤러 생성 및 블루프린터 연동
    - LobbyPlayerController (마우스 커서 활성화, UI 입력 모드 설정)
    
    - C++ 클래스 상속받은 GM_Lobby/PC_PlayerController 생성

---
+++ Source/Paradise/Private/Framework/Lobby/LobbyGameMode.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Framework/Lobby/LobbyGameMode.h"
+
+ALobbyGameMode::ALobbyGameMode()
+{
+}
+
+void ALobbyGameMode::StartGame()
+{
+	//[ToDo] GameInstance 만들면 다시 처리
+}
